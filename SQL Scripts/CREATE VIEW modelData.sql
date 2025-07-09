23f788b88eba5909ed24319f555c997b33c31359
Javen W. <javenwalker1996@outlook.com>
2025-07-09 10:03:11 -0500

    Cleaned the structure of my project.

--- SQL Scripts/CREATE VIEW modelData.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW modelData AS
-SELECT 
-	pbp.wp,
-	pbp.epa,
-	pbp.game_id,
-	ro.position
-FROM rawpbp pbp
-LEFT JOIN rawoff ro ON pbp.passer_id = ro.player_id
-GROUP BY QB1, pbp.epa, pbp.wp, pbp.game_id;
